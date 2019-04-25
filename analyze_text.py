28ee52a453782c5009f6827fec7200b9773dec47
Brady Niemitalo Woods <49171897+BDPNC101@users.noreply.github.com>
2019-04-25 14:36:53 -0600

    Update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -1,4 +1,15 @@
-# Remember to reach out for help after your own due diligence
-
 def analyze_text(text):
-    # Your code here
+#preliminaries
+    n = 0
+    e_n = text.count("e") + text.count("E") #gives count of instantiations of 'e' or "E" in text
+#analysis of text
+    for char in text: #gives count of alphabetic characters
+        if char.isalpha() == True:
+            n += 1
+        else:
+            pass
+#convert to percentage value
+    e_p = str(round(n / e_n, 2))
+#return
+    string = "The text contains " + str(n) + " alphabetic characters, of which " + str(e_n) + " (" + str(e_p) + "%) are 'e'."
+    return string
